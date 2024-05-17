b7152c3e8c043e11182d9041f07287aaa2b5cac6
Documenter.jl <documenter@juliadocs.github.io>
2024-05-17 12:42:30 +0000

    build based on 5e3d97e

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SurvivalBase","category":"page"},{"location":"#SurvivalBase","page":"Home","title":"SurvivalBase","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SurvivalBase.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SurvivalBase]","category":"page"}]
+}
