f9e32c48fa6db22e61f5c72acbb84b65328fd696
Oskar Laverny <oskar.laverny@univ-amu.fr>
2024-06-08 17:44:13 +0200

    add_docs

--- src/Surv.jl
+++ src/Surv.jl
@@ -1,3 +1,14 @@
+"""
+    Surv(T::Symbol, Δ::Symbol)
+
+Create a `SurvTerm` object that holds a tuple of variables `(T,Δ)` in the construction of a formula, usually on the left-hand-side, representing a right-censored output time `T` with its status indicatrix `Δ`: Δ being true means that the time was indeed observed, while false means censoring. 
+
+This information, usually on the left hand side a formula, has then a behavior that may depend on the estimator. It allows to use formulas from [`StatsModels.jl`](https://github.com/JuliaStats/StatsModels.jl) as follow: 
+
+    @formula(Surv(time,status) ~ covariate1 + covariate 2)
+
+This usage is common in, e.g., cox models. However, note that the *meaning* of the formula heavily depends on the model : for hazard regession and maximum likelyhood estimation, this does not mean the same thing. 
+"""
 Surv(T::Float64, Δ::Bool) = (T, Δ)
 struct SurvTerm{X, Y} <: AbstractTerm
     T::X
